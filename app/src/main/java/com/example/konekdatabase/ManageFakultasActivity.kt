d3e345bbd998aafd6fdde9cf565e6d8c71095d6b
Muldani <muldani@student.uty.ac.id>
2019-12-05 22:57:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/konekdatabase/ManageFakultasActivity.kt
@@ -0,0 +1,12 @@
+package com.example.konekdatabase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ManageFakultasActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manage_fakultas)
+    }
+}
